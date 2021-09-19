e76f769489ed3c7f8db80e390508ae58a7f59527
Martin M <48408674+martinGITHUBER@users.noreply.github.com>
2021-09-19 10:32:32 -0400

    Update utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -9,6 +9,15 @@ class Utils {
 		static Vector2 interpolate_vectors(Vector2 a, Vector2 b, double alpha) {
 			return Vector2{ (float)(b.x * alpha + a.x * (1.0 - alpha)), (float)(b.y * alpha + a.y * (1.0 - alpha)) };
 		}
+		static Vector2 subtract_vectors(Vector2 a, Vector2 b) {
+			return Vector2{ a.x - b.x, a.y - b.y };
+		}
+		static Vector2 add_vectors(Vector2 a, Vector2 b) {
+			return Vector2{ a.x + b.x, a.y + b.y };
+		}
+		static Vector2 scale_vector(Vector2 a, double scale) {
+			return Vector2{ (float)(a.x * scale), (float)(a.y * scale) };
+		}
 		static Vector2 window_center() {
 			return Vector2{ GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
 		}
