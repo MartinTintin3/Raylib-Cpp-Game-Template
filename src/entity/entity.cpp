a8365d1d32359431b1ce3d60941988a355391e08
Martin M <48408674+martinGITHUBER@users.noreply.github.com>
2021-09-26 10:49:43 -0400

    Update entity.cpp

--- src/entity/entity.cpp
+++ src/entity/entity.cpp
@@ -5,7 +5,7 @@
 #include "constants.hpp"
 #include "utils.hpp"
 
-Entity::Entity(Vector2 position, Vector2 size, double speed, Color color, Health health):
+Entity::Entity(Vector2 position, Vector2 size, float speed, Color color, Health health):
 		position{ position }, previous_position{ position.x, position.y },
 		velocity{ 0, 0 }, acceleration{ 0, 0 }, speed{ speed },
 		size{ size }, color{ color }, health{ health } { }
